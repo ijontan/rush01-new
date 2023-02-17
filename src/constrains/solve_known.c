20b3224f4e2b69c53253136260de6e5d587f4f1a
ijon <itan@student.42kl.edu.my>
2023-02-17 19:46:07 +0800

    first commit

---
+++ src/constrains/solve_known.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve_known.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 19:22:54 by itan              #+#    #+#             */
+/*   Updated: 2023/02/17 19:45:02 by itan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	add_equal_size(int **table, int i, int j)
+{
+}
+
+int	seen_all(int **table, int i, int j, int size)
+{
+}
+
+int	solve_known(int **table, int size)
+{
+	int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (table[i + 1][0] == size)
+			seen_all(table, i + 1, 0, size);
+		if (table[i + 1][size + 1] == size)
+			seen_all(table, i + 1, size + 1, size);
+		if (table[i + 1][0] + table[i + 1][size + 1] == size + 1)
+			add_equal_size(table, )
+				i++;
+	}
+}
---
+++ src/parse/parse_table.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_table.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 18:34:35 by itan              #+#    #+#             */
+/*   Updated: 2023/02/17 19:05:21 by itan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rush01.h"
+
+int	**create_table(int size)
+{
+	int	**dst;
+	int	i;
+
+	i = 0;
+	dst = (int **)malloc(sizeof(int *) * (size + 2));
+	if (!dst)
+		return (NULL);
+	while (i < size + 2)
+	{
+		dst[i] = (int *)malloc(sizeof(int) * (size + 2));
+		if (!dst[i++])
+			return (NULL);
+	}
+	return (dst);
+}
+
+void	free_table(int **table)
+{
+	int	i;
+
+	i = 0;
+	while (i < 6)
+		free(table[i++]);
+	free(table);
+}
+
+int	**parse_table(char const **av, int size)
+{
+	int	**dst;
+	int	i;
+
+	dst = create_table(size);
+	i = 0;
+	while (i < size)
+		dst[0][(i++) + 1] = atoi(*(av++));
+	i = 0;
+	while (i < size)
+		dst[5][(i++) + 1] = atoi(*(av++));
+	i = 0;
+	while (i < size)
+		dst[(i++) + 1][0] = atoi(*(av++));
+	i = 0;
+	while (i < size)
+		dst[(i++) + 1][0] = atoi(*(av++));
+	return (dst);
+}
